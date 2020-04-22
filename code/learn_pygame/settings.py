73e9628095ee90bdf4b75cc0bc0f221ffa0f438b
ppka0606 <ppka0606@gmail.com>
2020-04-22 19:19:22 +0800

    实现了非整数的移动速度

--- code/learn_pygame/settings.py
+++ code/learn_pygame/settings.py
@@ -3,4 +3,4 @@ class Settings():
         self.screen_width = 800
         self.screen_height = 640
         self.background_color = (230,230,230)
-        
+        self.ship_speed_factor = 1.5
--- code/learn_pygame/ship.py
+++ code/learn_pygame/ship.py
@@ -2,16 +2,18 @@ import pygame
 
 class Ship():
 
-    def __init__(self,screen):
+    def __init__(self,ai_settings,screen):
         """
         初始化飞船并设定初始位置
         """
 
         self.screen = screen
+        self.ai_settings = ai_settings
+
         self.image = pygame.image.load(r"resource\alienInvasion\ship.bmp")
 
         self.image.set_colorkey((255,255,255))
-        # 这个设置可以使图片的白色背景编程透明的
+            # 这个设置可以使图片的白色背景编程透明的
         
         self.rect = self.image.get_rect()
         self.scree_rect = screen.get_rect()
@@ -20,9 +22,13 @@ class Ship():
         self.rect.centerx = self.scree_rect.centerx
         self.rect.bottom = self.scree_rect.bottom
 
+        self.center = float(self.rect.centerx)
+            # 存储非整数值(因为rect的centerx等属性只能存储int,所以需要特殊处理)
+
         # 设置初始的移动状态
         self.moving_right = False
         self.moving_left = False
+
     def blitme(self):
         """
         图像绘制再指定位置
@@ -32,7 +38,13 @@ class Ship():
 
     def update(self):
         if self.moving_right == True:
-            self.rect.centerx += 1
+            self.center += self.ai_settings.ship_speed_factor
         
         if self.moving_left == True:
-            self.rect.centerx -= 1
+            self.center -= self.ai_settings.ship_speed_factor
+
+        # 使用存储了float类型的center属性来更新rect对象
+        self.rect.centerx = self.center
+            # 可以试想一下,每次center存储的是准确值,而赋给rect.centerx后只会保留整数部分
+            # 相当于用"3像素/2帧"的效果实现了"1.5像素/帧"的预期速度
+            # 由于1像素很小,每次移动距离很大,所以在移动时可能产生的0.5像素的偏差就无关紧要了
