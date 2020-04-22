efa30ad53def3e3c53f63cea5f04af08c18d7649
ppka0606 <ppka0606@gmail.com>
2020-04-22 23:20:03 +0800

    添加了Bullet类中用到的常数

--- code/learn_pygame/settings.py
+++ code/learn_pygame/settings.py
@@ -1,6 +1,17 @@
 class Settings():
     def __init__(self):
+        """
+        存储一些常数,便于跨文件引用
+        """
+
+        # 背景设置
         self.screen_width = 800
         self.screen_height = 640
         self.background_color = (230,230,230)
         self.ship_speed_factor = 1.5
+
+        # 设置子弹的属性
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
