d43a231f62adbb115cca11b91f182b6c5789a90c
ppka0606 <ppka0606@gmail.com>
2020-04-22 23:59:17 +0800

    现在可以使用空格键发射子弹了

--- code/learn_pygame/alien_invasion.py
+++ code/learn_pygame/alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 
 import game_function as gf
 from settings import Settings
@@ -13,10 +14,12 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     
     ship = Ship(ai_settings,screen)
+    bullets = Group()
 
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings,screen,ship)
+        bullets.update()
+        gf.update_screen(ai_settings,screen,ship,bullets)
 
 run_game()
--- code/learn_pygame/game_function.py
+++ code/learn_pygame/game_function.py
@@ -2,11 +2,17 @@
 import pygame
 import os
 
-def check_keydown_events(event,ship):
+from bullet import Bullet
+
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     if event.key == pygame.K_LEFT:
         ship.moving_left = True
+    if event.key == pygame.K_SPACE:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+            # 按住空格键添加一颗新的子弹进去
 
 def check_keyup_events(event,ship):
     if event.key == pygame.K_RIGHT:
@@ -14,7 +20,7 @@ def check_keyup_events(event,ship):
     if event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     for event in pygame.event.get():
         # 退出事件
         if event.type == pygame.QUIT:
@@ -24,13 +30,17 @@ def check_events(ship):
 
         # 键盘事件
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event,ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event,ship)
 
-def update_screen(ai_settings,screen,ship):
+def update_screen(ai_settings,screen,ship, bullets):
 
     screen.fill(ai_settings.background_color)# 用指定的rgb元组来填充底色
+
+    for bullet in bullets:
+        bullet.draw_bullet()
+
     ship.blitme()
 
     pygame.display.flip()
