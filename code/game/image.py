6aa02174ce8dbcf440c005ea672571d6fb31aa93
ppka0606 <ppka0606@gmail.com>
2020-06-08 00:37:49 +0800

    删去了image的冗余子类，现在所有图像通过image直接加载

--- code/game/image.py
+++ code/game/image.py
@@ -2,27 +2,16 @@ import pygame
 
 class Image():
     """
-    以此为父类创建图像的管理
+    图像的管理
     """
     def __init__(self, image_path):
         self.image = pygame.image.load(image_path)
         
         self.image.set_colorkey((255,255,255))
+
+        self.height = self.image.get_rect().height
         # self._rect = self.image.get_rect()
 
     def blit_image(self, screen, posx, posy):
         screen.blit(self.image, (posx,posy))
     
-class ImageWall(Image):
-    """
-    迷宫的墙的图片
-    """
-    def __init__(self):
-        super(ImageWall, self).__init__(r"resource\mingingGame\game\stonewall.bmp")
-
-class ImageSoil(Image):
-    """
-    迷宫的地面图片
-    """
-    def __init__(self):
-        super(ImageSoil, self).__init__(r"resource\mingingGame\game\soil.bmp")
