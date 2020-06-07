0dc89d757f46599eaf62612302ef2c93cd5019bd
ppka0606 <ppka0606@gmail.com>
2020-06-08 00:40:17 +0800

    添加了绘制多种界面的函数，以及各种状态之间的切换

--- code/game/game_functions.py
+++ code/game/game_functions.py
@@ -1,53 +1,201 @@
 import pygame
 import os
 
-from image import ImageWall, ImageSoil
+from image import Image
 from const import Const
 from button import Button
+from status import Status
+
+def check_exit(screen, status):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            status.close = True
+        if event.type == pygame.MOUSEMOTION:
+            mouse_x,mouse_y = pygame.mouse.get_pos()
+            status.buttons["confirm_exit"].selected(mouse_x, mouse_y)
+            status.buttons["cancel_exit"].selected(mouse_x, mouse_y)
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if status.buttons["cancel_exit"].selected(mouse_x, mouse_y):
+                status.close = False
+            if status.buttons["confirm_exit"].selected(mouse_x, mouse_y):
+                os._exit(0)
+
+def check_login(screen, status):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            status.close = True
+        if event.type == pygame.MOUSEMOTION:
+            mouse_x,mouse_y = pygame.mouse.get_pos()
+            status.buttons["start"].selected(mouse_x, mouse_y)
+            status.buttons["help"].selected(mouse_x, mouse_y)
+            status.buttons["player1"].selected(mouse_x, mouse_y)
+            status.buttons["player2"].selected(mouse_x, mouse_y)
+            status.buttons["player3"].selected(mouse_x, mouse_y)
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if status.buttons["player1"].selected(mouse_x, mouse_y):
+                status.buttons["confirm_player"].update_text("已选择 饭团")
+
+                status.buttons["player1"].lock()
+                status.buttons["player2"].unlock()
+                status.buttons["player3"].unlock()
+
+                status.player_kind = Status.PLAYER1
+
+            if status.buttons["player2"].selected(mouse_x, mouse_y):
+                status.buttons["confirm_player"].update_text("已选择 蛋黄")
+
+                status.buttons["player1"].unlock()
+                status.buttons["player2"].lock()
+                status.buttons["player3"].unlock()
+
+                status.player_kind = Status.PLAYER2
+            if status.buttons["player3"].selected(mouse_x, mouse_y):
+                status.buttons["confirm_player"].update_text("已选择 栗子")
+
+                status.buttons["player1"].unlock()
+                status.buttons["player2"].unlock()
+                status.buttons["player3"].lock()
+
+                status.player_kind = Status.PLAYER3
+            if status.buttons["help"].selected(mouse_x, mouse_y):
+                status.interface = Status.INTERFACE_HELP
+
+            if status.buttons["start"].selected(mouse_x, mouse_y)and status.player_kind != Status.PLAYERBEGIN:
+                status.interface = Status.INTERFACE_GAME
+                status.levelup()
+                return
+
+def check_help(screen, status):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            status.close = True
+        if event.type == pygame.MOUSEMOTION:
+            mouse_x,mouse_y = pygame.mouse.get_pos()
+            status.buttons["return"].selected(mouse_x, mouse_y)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x,mouse_y = pygame.mouse.get_pos()
+            if status.buttons["return"].selected(mouse_x, mouse_y):
+                status.interface = Status.INTERFACE_LOGIN
+                return
+
+def check_restart(screen, status):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            status.close = True
+        if event.type == pygame.MOUSEMOTION:
+            mouse_x,mouse_y = pygame.mouse.get_pos()
+            status.buttons["retry"].selected(mouse_x, mouse_y)
+            status.buttons["returnmain"].selected(mouse_x, mouse_y)
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if status.buttons["returnmain"].selected(mouse_x, mouse_y):
+                status.interface = Status.INTERFACE_LOGIN
+                return
+            
+def check_game(screen, status):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            status.close = True
 
 def check_events(screen, status):
     """
     事件监听
     """
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            os._exit(0)
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_q:
-                os._exit(0)
+    if status.close:
+        check_exit(screen, status)
+        return
+    if status.interface == Status.INTERFACE_LOGIN:
+        check_login(screen, status)
+    if status.interface == Status.INTERFACE_HELP:
+        check_help(screen, status)
+    if status.interface == Status.INTERFACE_RESTART:
+        check_restart(screen, status)
+    if status.interface == Status.INTERFACE_GAME:
+        check_game(screen, status)
 
-def update_screen(screen, status):
-    draw_map(screen, status.maze, (16, 8))
-    for button in status.buttons.values():
-        button.blit_button()
+def draw_exit(screen, status):
+    screen.fill(Const.COLOR_DARKGRAY)
+    status.buttons["ask_exit"].blit_button()
+    status.buttons["confirm_exit"].blit_button()
+    status.buttons["cancel_exit"].blit_button()
+
+def draw_login(screen, status):
+    screen.fill(Const.COLOR_LIGHTGRAY)
+    status.buttons["start"].blit_button()
+    status.buttons["help"].blit_button()
+    status.buttons["player1"].blit_button()
+    status.buttons["player2"].blit_button()
+    status.buttons["player3"].blit_button()
+    status.buttons["confirm_player"].blit_button()
+
+    status.images["icon1"].blit_image(status.screen, 130, 130)
+    status.images["icon2"].blit_image(status.screen, 360, 130)
+    status.images["icon3"].blit_image(status.screen, 590, 130)
+
+def draw_help(screen, status):
+    screen.fill(Const.COLOR_LIGHTGRAY)
+    status.buttons["return"].blit_button()
+    status.buttons["help_title"].blit_button()
+
+    status.images["icon1"].blit_image(status.screen, 130, 130)
+    status.images["icon2"].blit_image(status.screen, 360, 130)
+    status.images["icon3"].blit_image(status.screen, 590, 130)
+
+    status.buttons["introduction1"].blit_button()
+    status.buttons["introduction2"].blit_button()
+    status.buttons["introduction3"].blit_button()
+    status.buttons["introduction4"].blit_button()
+    status.buttons["introduction5"].blit_button()
+    status.buttons["introduction6"].blit_button()
+
+def draw_restart(screen, status):
+    screen.fill(Const.COLOR_LIGHTGRAY)
+    status.buttons["gameover"].blit_button()
+    status.buttons["retry"].blit_button()
+    status.buttons["returnmain"].blit_button()
+
+def draw_game(screen, status):
+    screen.fill(Const.COLOR_LIGHTGRAY)
+    status.buttons["time"].blit_button()
+    status.buttons["score"].blit_button()
+    status.images["scorediamond"].blit_image(status.screen, 0, Const.SCREEN_HEIGHT - 83)
+    status.images["timediamond"].blit_image(status.screen, 170, Const.SCREEN_HEIGHT - 80)
+    status.buttons["num_scorediamond"].blit_button()
+    status.buttons["num_timediamond"].blit_button()
+
+    # 绘制迷宫
+    # 尽量使人物处于中间位置，因此就需要计算需要呈现于图中的区域
+    if status.player.actual_posx <= Const.MAZE_SQUARE_PIXEL * Const.MAZE_DISPLAY_WIDTH_SQUARE / 2 and status.player.actual_posy <= Const.MAZE_SQUARE_PIXEL * Const.MAZE_DISPLAY_HEIGHT_SQUARE / 2: # 在左上角区域
+        for i in range(Const.MAZE_DISPLAY_HEIGHT_SQUARE):
+            for j in range(Const.MAZE_DISPLAY_WIDTH_SQUARE):
+                posx = Const.GAME_INTERFACE_MAINAREA_POSX + j * Const.MAZE_SQUARE_PIXEL
+                posy = Const.GAME_INTERFACE_MAINAREA_POSY + i * Const.MAZE_SQUARE_PIXEL
+                if status.maze[i][j] == Const.MAZE_WALL:
+                    status.images["wall"].blit_image(status.screen, posx, posy)
+                else:
+                    status.images["road"].blit_image(status.screen, posx, posy)
+        
+        sprite_image = status.player.images[status.player.direction - 1]
+        sprite_posx = int(status.player.actual_posx) + Const.GAME_INTERFACE_MAINAREA_POSX
+        sprite_posy = int(status.player.actual_posy) + Const.GAME_INTERFACE_MAINAREA_POSY - sprite_image.height
+        sprite_image.blit_image(status.screen, sprite_posx, sprite_posy) # 此时没有相对移位，所以就把精灵放在绝对位置就好
+
+def draw(screen, status):
+    if status.close:
+        draw_exit(screen, status)
+        return
+    if status.interface == Status.INTERFACE_LOGIN:
+        draw_login(screen, status)
+    if status.interface == Status.INTERFACE_HELP:
+        draw_help(screen, status)
+    if status.interface == Status.INTERFACE_RESTART:
+        draw_restart(screen, status)
+    if status.interface == Status.INTERFACE_GAME:
+        draw_game(screen, status)
 
 
-def draw_login(screen):
-    pass
-
-def draw_map(screen, maze, focus):
-    """
-    根据传入的level绘制map
-
-    focus表示游戏主界面的中心焦点，因为再界面上显示的游戏不是直接将整个地图绘制出来
-    """
-    image_wall = ImageWall()
-    image_soil = ImageSoil()
-
-    beginx = focus[0] - (Const.MAZE_DISPLAY_WIDTH_SQUARE - 1) // 2
-    endx = focus[0] + (Const.MAZE_DISPLAY_WIDTH_SQUARE + 1) // 2
-    beginy = focus[1] - (Const.MAZE_DISPLAY_HEIGHT_SQUARE) // 2
-    endy = focus[1] + (Const.MAZE_DISPLAY_HEIGHT_SQUARE + 1) // 2
-    
-    posy = 0
-    posx = 0
-    for i in range(beginy, endy):
-        posx = 0
-        for j in range(beginx, endx):
-            if maze[i][j] == Const.MAZE_WALL:
-                image_wall.blit_image(screen, posx * Const.MAZE_SQUARE_PIXEL + Const.GAME_INTERFACE_MAINAREA_POSX, posy * Const.MAZE_SQUARE_PIXEL + Const.GAME_INTERFACE_MAINAREA_POSY)
-            else:
-                image_soil.blit_image(screen, posx * Const.MAZE_SQUARE_PIXEL + Const.GAME_INTERFACE_MAINAREA_POSX, posy * Const.MAZE_SQUARE_PIXEL + Const.GAME_INTERFACE_MAINAREA_POSY)
-            posx += 1
-        posy += 1
-    
--- code/game/status.py
+++ code/game/status.py
@@ -1,6 +1,8 @@
 from button import Button
 from const import Const
 from player import Player
+from image import Image
+from maze import Maze
 
 class Status():
     """
@@ -13,13 +15,15 @@ class Status():
     INTERFACE_PAUSE = 2                 # 暂停界面
     INTERFACE_HELP = 3                  # 帮助菜单
     INTERFACE_CONFIRM_EXIT = 4          # 确认退出
-    INTERFACE_RESULT = 5                # 结算界面，显示总分以及历史最高分
+    INTERFACE_RESULT = 5                # 结算界面分
+    INTERFACE_RESTART = 6               # 某一关gameover后重开
 
     LEVELBEGIN = 0
     LEVEL1 = 1
     LEVEL2 = 2
     LEVEL3 = 3
 
+    PLAYERBEGIN = 0
     PLAYER1 = 1
     PLAYER2 = 2
     PLAYER3 = 3
@@ -32,6 +36,7 @@ class Status():
         self.screen = screen
 
         self.interface = self.INTERFACE_LOGIN
+        self.close = False
         self.level = self.LEVELBEGIN
         self.user = r"Input player name:"
 
@@ -41,25 +46,62 @@ class Status():
         self.key_up = False
         self.key_down = False
 
-        self.map = None
+        self.maze = None
         self.distribution = None
         self.add_buttons()
 
-        self.player_kind = 1
+        self.player_kind = self.PLAYERBEGIN
 
         self.player = None
+        self.images = {}
+        self.add_images()
+
+    def levelup(self):
+        self.level += 1
+        self.maze = Maze(self.level).map
+        self.player = Player(self.player_kind, self.maze)
 
     def add_buttons(self):
         self.buttons = {}
+        # 退出界面
+        self.buttons["ask_exit"] = Button(self.screen, "确认退出?", height = 60, text_color = Const.COLOR_RED, fontsize = 48, button_color = Const.COLOR_DARKGRAY, posy = 220)
+        self.buttons["confirm_exit"] = Button(self.screen, "确认", height = 50, text_color = Const.COLOR_BLACK, button_color = Const.COLOR_DARKGRAY, posy = 400, posx = 150, fontsize = 36)
+        self.buttons["cancel_exit"] = Button(self.screen, "取消", height = 50, text_color = Const.COLOR_BLACK, button_color = Const.COLOR_DARKGRAY, posy = 400, posx = 450, fontsize = 36, selected_color = Const.COLOR_BLUE)
 
-        self.buttons["score"] = Button(self.screen, "Score: 00000", posx = 0, posy = 0, fontsize = 24) # 添加左上角分数按钮
+        # 开始界面
+        self.buttons["start"] = Button(self.screen, "开始游戏", posy = 450, fontsize = 48, button_color = Const.COLOR_LIGHTGRAY, text_color = Const.COLOR_BLACK)
+        self.buttons["help"] = Button(self.screen, "帮助菜单" , posy = 520, fontsize = 42, button_color = Const.COLOR_LIGHTGRAY, text_color = Const.COLOR_BLACK)
+        self.buttons["player1"] = Button(self.screen, "饭团" , width =120, height = 40, posx = 110, posy = 270, button_color = Const.COLOR_LIGHTGRAY, text_color = Const.COLOR_GREEN, fontsize = 28, selected_color = Const.COLOR_BLUE)
+        self.buttons["player2"] = Button(self.screen, "蛋黄" , width =120, height = 40, posx = 340, posy = 270, button_color = Const.COLOR_LIGHTGRAY, text_color = Const.COLOR_GREEN, fontsize = 28, selected_color = Const.COLOR_BLUE)
+        self.buttons["player3"] = Button(self.screen, "栗子" , width =120, height = 40, posx = 570, posy = 270, button_color = Const.COLOR_LIGHTGRAY, text_color = Const.COLOR_GREEN, fontsize = 28, selected_color = Const.COLOR_BLUE)
+        self.buttons["confirm_player"] = Button(self.screen, "选择角色后开始游戏", fonstize = 36, button_color = Const.COLOR_DARKGRAY, text_color = Const.COLOR_BLUE, posy = 350, fontsize = 28, width = 260)
 
-        self.buttons["time"] = Button(self.screen, "Time: 000s",posx = Const.SCREEN_WIDTH - 130, posy = 0, width = 130,fontsize = 24) # 右上角剩余时间
-        
-        self.buttons["start"] = Button(self.screen, "开始游戏", posy = 250, fonsize = 40)
-        self.buttons["help"] = Button(self.screen, "帮助菜单" , posy = 350, fontsize = 40)
+        # 帮助界面
+        self.buttons["help_title"] = Button(self.screen, "帮 助", posy = 50, fontsize = 48, button_color = Const.COLOR_LIGHTGRAY, text_color = Const.COLOR_BLACK)
+        self.buttons["return"] = Button(self.screen, "返回主界面", posy = 550, fontsize = 40, button_color = Const.COLOR_LIGHTGRAY, text_color = Const.COLOR_BLACK)
+        self.buttons["introduction1"] = Button(self.screen, "移动速度快        移动速度慢      移动速度适中", posy = 250, posx = 110, width = 580, height = 50, button_color = Const.COLOR_LIGHTGRAY, text_color = Const.COLOR_BLACK, fontsize = 28)
+        self.buttons["introduction2"] = Button(self.screen, "挖宝速度慢        挖宝速度快      挖宝速度适中", posy = 300, posx = 110, width = 580, height = 50, button_color = Const.COLOR_LIGHTGRAY, text_color = Const.COLOR_BLACK, fontsize = 28)
+        self.buttons["introduction3"] = Button(self.screen, "   遇到宝石概率低        遇到宝石概率适中     遇到宝石概率逐渐提升", posy = 350, posx = 110, width = 580, height = 50, button_color = Const.COLOR_LIGHTGRAY, text_color = Const.COLOR_RED, fontsize = 22)
+        self.buttons["introduction4"] = Button(self.screen, "使用键盘的方向键在迷宫中移动", posy = 420, posx = 110, width = 580, height = 40, button_color = Const.COLOR_DARKGRAY, text_color = Const.COLOR_BLACK, fontsize = 22)
+        self.buttons["introduction5"] = Button(self.screen, "在规定时间内走到终点，并挖取尽可能多的宝石来提高分数!", posy = 460, posx = 110, width = 580, height = 40, button_color = Const.COLOR_DARKGRAY, text_color = Const.COLOR_BLACK, fontsize = 22)
+        self.buttons["introduction6"] = Button(self.screen, "红色的宝石可以获得额外分数，绿色的宝石可以延长时间!", posy = 500, posx = 110, width = 580, height = 40, button_color = Const.COLOR_DARKGRAY, text_color = Const.COLOR_BLACK, fontsize = 22)
 
-        self.buttons["pause"] = Button(self.screen, "暂 停", width = 120, height = 40, posx = Const.SCREEN_WIDTH - 120, posy = Const.SCREEN_HEIGHT -40, fontsize = 30)
+        # 重开界面
+        self.buttons["gameover"] = Button(self.screen, "Game Over!", height = 60, text_color = Const.COLOR_RED, fontsize = 48, button_color = Const.COLOR_LIGHTGRAY, posy = 220)
+        self.buttons["retry"] = Button(self.screen, "重玩本关", height = 50, text_color = Const.COLOR_BLACK, button_color = Const.COLOR_LIGHTGRAY, posy = 400, posx = 150, fontsize = 36, selected_color = Const.COLOR_BLUE)
+        self.buttons["returnmain"] = Button(self.screen, "返回主界面", height = 50, text_color = Const.COLOR_BLACK, button_color = Const.COLOR_LIGHTGRAY, posy = 400, posx = 450, fontsize = 36, selected_color = Const.COLOR_BLUE)
 
-        self.buttons["exit_yes"] = Button(self.screen, "确认退出", width = 120,height = 40, posx = 200)
-        self.buttons["exit_no"] = Button(self.screen, "返回菜单" , width = 120, height = 40, posx =480)
+        # 游戏界面
+        self.buttons["score"] = Button(self.screen, "Score:00000", text_color = Const.COLOR_BLACK, button_color = Const.COLOR_LIGHTGRAY, fontsize = 32,posx = 0, posy = 0)
+        self.buttons["time"] = Button(self.screen, "Time:000s", text_color =Const.COLOR_BLACK, button_color = Const.COLOR_LIGHTGRAY, width = 200, fontsize = 32, posy = 0, posx = Const.SCREEN_WIDTH - 200)
+        self.buttons["num_scorediamond"] = Button(self.screen, "X000", text_color = Const.COLOR_BLACK, button_color = Const.COLOR_LIGHTGRAY,width = 60, fontsize = 32, posx = 60, posy = Const.SCREEN_HEIGHT - 76)
+        self.buttons["num_timediamond"] = Button(self.screen, "X000", text_color = Const.COLOR_BLACK, button_color = Const.COLOR_LIGHTGRAY, width = 60, fontsize = 32, posx = 230, posy = Const.SCREEN_HEIGHT - 76)
+
+    def add_images(self):
+        self.images["icon1"] = Image(Const.IMAGE_PLAYER1_ICON_PATH)
+        self.images["icon2"] = Image(Const.IMAGE_PLAYER2_ICON_PATH)
+        self.images["icon3"] = Image(Const.IMAGE_PLAYER3_ICON_PATH)
+        self.images["scorediamond"] = Image(Const.IMAGE_SCOREDIAMOND_PATH)
+        self.images["timediamond"] = Image(Const.IMAGE_TIMEDIAMOND_PATH)
+        self.images["road"] = Image(Const.IMAGE_ROAD_PATH)
+        self.images["wall"] = Image(Const.IMAGE_WALL_PATH)
