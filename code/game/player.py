00bf139fe13e8cbff27b99ea7fb027ae9e327528
ppka0606 <ppka0606@gmail.com>
2020-06-08 00:41:41 +0800

    删去了切换走路状态的部分，因为工程量实在太大，且素材非常难处理

--- code/game/player.py
+++ code/game/player.py
@@ -1,7 +1,7 @@
 import pygame
 from maze import Maze
 from const import Const
-
+from image import Image
 
 class Player():
     """
@@ -10,41 +10,51 @@ class Player():
     包括图片，位置，速度等信息
     """
 
-    STILL = 0
-    LEFT_FOOT = 1
-    RIGHT_FOOT = 2
-
-
     DIRECTION_UP    = 1
     DIRECTION_DOWN  = 2
     DIRECTION_LEFT  = 3
     DIRECTION_RIGHT = 4
 
     def __init__(self, player_kind, maze):
-        # 为了呈现一定的立体效果，player的图片(45, 30）可以覆盖一点墙体，
-        # player的实际高度和宽度(用于判定撞墙)为（30，30），见Cosnt
+        """
+        实现init函数的可复用，如失败/通关后退回到主界面
+        """
+        # player图像的实际高度可能和判定高度不完全相同，主要是为了体现可能的立体效果
+        # player的判定高度和宽度(用于判定撞墙)为（30，30），见Cosnt
         # 以左下角的坐标为基准进行计算,绘制时再转换为左上角
         self.player_kind = player_kind
         self.maze = maze
         self.speed = Const.PLAYER_SPEED[self.player_kind - 1]
+        self.initpos()
 
+        self.images = []
+        self.getimage()
+
+    def initpos(self):
         self.actual_posx = float(Const.MAZE_SQUARE_PIXEL)
         self.actual_posy = float(Const.MAZE_SQUARE_PIXEL + Const.PLAYER_HEIGHT_PIXEL)
         # 起始位置为左上角， 用float存储位置便于计算
-
         self.takeup = [None , None, None, None] # 标记占据的格子，玩家最多可以同时占据四个格子，需进行判定
         # 四个位置的顺序分别是 0 1
         #                    2 3
         self.check_takeup()
         
-        self.walkstatus = self.STILL
+        if self.maze != None:
+            if self.maze[1][2] == Const.MAZE_ROAD:
+                self.direction = self.DIRECTION_RIGHT
+            else:
+                self.direction = self.DIRECTION_DOWN
+                    # 避免初始的时候脸对着墙
 
-        if self.maze[1][2] == Const.MAZE_ROAD:
-            self.direction = self.DIRECTION_RIGHT
-        else:
-            self.direction = self.DIRECTION_DOWN
-                # 避免初始的时候脸对着墙
-
+    def getimage(self):
+        for i in range(1, 5):
+            path = r"resource\mingingGame\game\player"
+            path += str(self.player_kind)
+            path += "_"
+            path += str(i)
+            path += ".bmp"
+            self.images.append(Image(path))
+    
     def go_up(self):
         self.check_takeup()
 
@@ -52,10 +62,6 @@ class Player():
             self.direction = self.DIRECTION_UP
             self.walkstatus = self.STILL
             return # 单次按键只转向不移动
-
-        self.walkstatus = (self.walkstatus + 1) % 3
-        # 三种行走状态来回切换，达到动画的效果
-
         limit = 0
         if self.takeup[1] == None:  # 横向占1格
             if self.maze[self.takeup[0][0] - 1][self.takeup[0][1]] == Const.MAZE_ROAD:
@@ -76,9 +82,6 @@ class Player():
             self.direction = self.DIRECTION_DOWN
             self.walkstatus = self.STILL
             return 
-
-        self.walkstatus = (self.walkstatus + 1) % 3
-        # 三种行走状态来回切换，达到动画的效果
 
         limit = 0
         if self.takeup[1] == None:
@@ -114,9 +117,6 @@ class Player():
             self.walkstatus = self.STILL
             return
 
-        self.walkstatus = (self.walkstatus + 1) % 3
-        # 三种行走状态来回切换，达到动画的效果
-
         limit = 0
         if self.takeup[2] == None:  # 纵向占1格
             if self.maze[self.takeup[0][0]][self.takeup[0][1] - 1] == Const.MAZE_ROAD:
@@ -137,9 +137,6 @@ class Player():
             self.direction = self.DIRECTION_RIGHT
             self.walkstatus = self.STILL
             return
-
-        self.walkstatus = (self.walkstatus + 1) % 3
-        # 三种行走状态来回切换，达到动画的效果
 
         limit = 0
         if self.takeup[2] == None:
@@ -186,6 +183,9 @@ class Player():
         if row1 == row2:
             self.takeup[2] = None
             self.takeup[3] = None
+
+    def getmaze(self, maze):
+        self.maze = maze
 
 if __name__ == "__main__":
     maze = Maze(3).map
